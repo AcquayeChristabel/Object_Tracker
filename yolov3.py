84423163e245d35eb24ad9d4cc5ea991ba597c05
AcquayeChristabel <123964784+AcquayeChristabel@users.noreply.github.com>
2023-11-06 07:31:48 -0500

    Milestone2 code

--- yolov3.py
+++ yolov3.py
@@ -1,74 +1,82 @@
 import cv2
 import numpy as np
 import serial
+import time
 
-# Establish serial connection with Arduino
-arduino = serial.Serial('COM7', 115200, timeout=1)
+# Used 60 initially
+CAMERA_FOV_HORIZONTAL = 90  
+SERIAL_PORT = 'COM9'
+BAUD_RATE = 115200
+TIMEOUT = 1
+CONFIDENCE_THRESHOLD = 0.5
 
-# Function to send command to Arduino
-def send_command_to_arduino(command):
-    arduino.write(command.encode('utf-8'))
-# Load YOLO
-net = cv2.dnn.readNet(r"C:\\Users\\acqua\\Desktop\\HCI Project\\yolov3.weights", r"C:\\Users\\acqua\\Desktop\\HCI Project\\yolov3.cfg")
+
+arduino = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=TIMEOUT)
+
+
+net = cv2.dnn.readNet("yolov4-tiny.weights", "yolov4-tiny.cfg")  
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers().flatten()]
 
-# Load the COCO class labels our YOLO model was trained on
-classes = []
-with open(r"C:\\Users\\acqua\\Desktop\\HCI Project\\coco.names", "r") as f:
+with open("coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 
-# Start capturing video from the default camera
+
 cap = cv2.VideoCapture(0)
 
+def calculate_angle(deviation, frame_width):
+    angle_per_pixel = CAMERA_FOV_HORIZONTAL / frame_width
+    return deviation * angle_per_pixel
+
+reference_center_x = None  
+
+def send_angle_to_arduino(angle, direction):
+    command = f"{direction}:{angle}\n"  
+    arduino.write(command.encode('utf-8'))
+    arduino.flush()  
+
 while True:
-    # Capture frame-by-frame
     ret, frame = cap.read()
     if not ret:
         break
 
-    # Prepare the frame for YOLO detection
-    height, width, channels = frame.shape
-    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    height, width, _ = frame.shape
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), swapRB=True, crop=False)
     net.setInput(blob)
     outs = net.forward(output_layers)
 
-    # Process the detections
     for out in outs:
         for detection in out:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if confidence > 0.5:
-                # Object detected
+
+            if confidence > CONFIDENCE_THRESHOLD and classes[class_id] == "person":
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
                 h = int(detection[3] * height)
-                # Calculate the deviation from the center
-                deviation = center_x - (width // 2)
-                # Rectangle coordinates
+
+                # Bounding box
                 x = int(center_x - w / 2)
                 y = int(center_y - h / 2)
-
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                cv2.putText(frame, f"{classes[class_id]}: {int(confidence * 100)}%", (x, y + 30), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
 
-                # Send command to Arduino based on the deviation
-                if deviation < 0:
-                    # Object is to the left, move stepper to the left
-                    send_command_to_arduino('b')
-                elif deviation > 0:
-                    # Object is to the right, move stepper to the right
-                    send_command_to_arduino('a')
+                if reference_center_x is None:
+                    reference_center_x = center_x 
 
-    # Display the resulting frame with detections
+                deviation = center_x - reference_center_x
+                angle_difference = calculate_angle(deviation, width)
+                direction = 'L' if deviation < 0 else 'R'
+                send_angle_to_arduino(abs(angle_difference), direction)
+                print(f"Moved {abs(angle_difference):.2f} degrees to the {direction}")
+
+              
+                cv2.putText(frame, f"Moved: {abs(angle_difference):.2f} degrees {direction}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
     cv2.imshow('frame', frame)
-
-    # Break the loop when 'q' is pressed
     if cv2.waitKey(1) == ord('q'):
         break
 
-# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
